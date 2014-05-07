c15d1d745d7a4be64752c39bef77b299b438ae8d
Kacper Bielecki <kazjote@gmail.com>
2014-05-07 23:13:50 +0200

    CMake build system

--- test_app.sh
+++ test_app.sh
@@ -1,35 +1,3 @@
-valac \
-    --pkg gtk+-3.0 \
-    --pkg gee-1.0 \
-    --pkg libxml-2.0 \
-    --pkg libsoup-2.4 \
-    --pkg gio-2.0 \
-    rtm/rtm.vala \
-    rtm/requester.vala \
-    rtm/authenticator.vala \
-    rtm/response.vala \
-    rtm/frob.vala \
-    rtm/token.vala \
-    rtm/timeline.vala \
-    rtm/task_list.vala \
-    rtm/task_serie.vala \
-    rtm/task.vala \
-    src/http_proxy_interface.vala \
-    src/http_proxy.vala \
-    src/gcowboy.vala \
-    src/queue_message.vala \
-    src/queue_processor.vala \
-    src/rtm_wrapper.vala \
-    src/views/task.vala \
-    src/views/task_list.vala \
-    src/views/notification_area.vala \
-    src/views/new_task.vala \
-    src/models/task.vala \
-    src/models/task_repository.vala \
-    --Xcc=-w \
-    --thread \
-    -g \
-    -o test_app && \
-./test_app
+cmake . && make && ./gcowboy
 
 # vim: ts=4 sw=4
--- test_rtm.sh
+++ test_rtm.sh
@@ -1,23 +1,3 @@
-valac \
-    --pkg gtk+-3.0 \
-    --pkg gee-1.0 \
-    --pkg libxml-2.0 \
-    rtm/test/test.vala \
-    rtm/test/authenticator_test.vala \
-    rtm/test/response_test.vala \
-    rtm/rtm.vala \
-    rtm/requester.vala \
-    rtm/authenticator.vala \
-    rtm/response.vala \
-    rtm/frob.vala \
-    rtm/token.vala \
-    rtm/timeline.vala \
-    rtm/task_list.vala \
-    rtm/task_serie.vala \
-    rtm/task.vala \
-    src/http_proxy_interface.vala \
-    --Xcc=-w \
-    -o test_rtm && \
-GCOWBOY_ENV=test ./test_rtm
+cmake . && make && GCOWBOY_ENV=test ./test_rtm
 
 # vim: ts=4 sw=4
